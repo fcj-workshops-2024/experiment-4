169c79e81a264eba3a014e103a43a965fcd0d15a
tulna07 <tu.lna07@gmail.com>
2024-09-19 17:31:00 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+DEFAULT=1
+
+def get_1s(n=DEFAULT):
+
+    '''
+        TODO: return an array of 1s
+    '''
+
+    return [1] * n
---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+from main import get_1s
+
+class TestGet1sFunction(unittest.TestCase):
+    
+    def test_with_10(self):
+        """Test the function with 10."""
+        result = get_1s(10)
+        expected_result = [1] * 10
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
